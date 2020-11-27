8b7851400699428b4c6dfdacd5fbe25c1d6612f6
Coti <ccnasisi@gmail.com>
2020-11-27 19:55:44 +0100

    Average´s file cleanedleaned

--- average.cs
+++ average.cs
@@ -1,20 +1,30 @@
+
+/*This program is going to tell us the average of the numbers who user 
+introduce until user enters "end".*/
+
+
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+
+public class Average {
+
+	public static void Main() {
+		double total = 0, mark, counter = 0, average;
+		string userTyping;
+		
+		Console.Write("Insert a mark: ");
+		userTyping = Console.ReadLine();	
+	
+		while (userTyping != "end") {  
+			mark = Convert.ToDouble(userTyping);
+			counter++;   
+			total += mark;  
+			Console.Write("Insert a mark: ");
+			userTyping = Console.ReadLine();
+		}
+		
+		if (counter != 0) {
+			average = total / counter;  
+			Console.WriteLine("The average is {0}.",average);
+		}
+	}
 }
